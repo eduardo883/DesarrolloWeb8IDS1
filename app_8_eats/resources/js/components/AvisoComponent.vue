c541bd5ab1fb43dc421652a5b16b4a9bdb3f746a
eduardo883 <eduardomagdielpadilla@gmail.com>
2021-07-06 18:46:43 -0500

    Creando Vista Avisos de Privacidad

---
+++ app_8_eats/resources/js/components/AvisoComponent.vue
@@ -0,0 +1,50 @@
+<template>
+
+    <div>
+
+    <div class="header bg-gradient-primary pt-5 pt-md-6">
+    <div class="container-fluid">
+    
+        <!-- Header -->
+        <div class="header pb-6">
+        <div class="container-fluid">
+            <div class="header-body">
+            <div class="row align-items-center py-4">
+                <div class="col-lg-6 col-7">
+                <h6 class="h2 text-white d-inline-block mb-0">Aviso de Privacidad</h6>
+                <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
+                    <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
+                        <li class="breadcrumb-item"><a><i class="fas fa-home"></i></a></li>
+                        <li class="breadcrumb-item"><a>Inicio</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Aviso de Privacidad</li>
+                    </ol>
+                </nav>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
+
+    <p>De conformidad con lo establecido en la Ley Federal de Protección de Datos Personales en Posesión de los Particulares y al capítulo VI del 
+        Título V de las Políticas Corporativas Responsables en Protección de Datos, ponemos a su disposición nuestro
+        Aviso de Privacidad WEB, con la finalidad de hacer de su conocimiento el tratamiento que daremos a sus Datos
+        Personales, así como los derechos que puede ejercer como titular de sus datos personales. Les solicitamos
+        leer cuidadosamente, ya que la sola disposición del presente Aviso de Privacidad sin que usted se oponga, 
+        otorga su consentimiento tácito para permitirnos tratar sus datos personales con base a lo establecido en el presente Aviso de Privacidad: </p>
+    <br>
+    <p> I. RESPONSABLE DEL USO Y PROTECCIÓN DE SUS DATOS: </p>
+    <br>
+    <p> WeDev con domicilio en: Tecámac, Edo. de México y como responsable del tratamiento de sus datos personales, hace de su conocimiento 
+        que todos sus datos personales son tratados de forma estrictamente confidencial por lo que al proporcionarlos, tales como: 
+    </p>
+
+    <br>
+
+    <p>II. DATOS PERSONALES QUE PODEMOS OBTENER: </p>
+    <br>
+    <ul></ul>
+
+    </div>
+</template>
